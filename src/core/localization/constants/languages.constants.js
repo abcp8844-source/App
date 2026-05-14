641bcf84b107ffca6062ab9657fb900d6ba2e154
abcp8844-source <abcp8844@gmail.com>
2026-05-14 13:23:29 +0700

    Create languages.constants.js

---
+++ src/core/localization/constants/languages.constants.js
@@ -0,0 +1,26 @@
+export const LANGUAGES = {
+  en: {
+    name: "English",
+    rtl: false,
+  },
+
+  ar: {
+    name: "Arabic",
+    rtl: true,
+  },
+
+  th: {
+    name: "Thai",
+    rtl: false,
+  },
+
+  zh: {
+    name: "Chinese",
+    rtl: false,
+  },
+
+  tr: {
+    name: "Turkish",
+    rtl: false,
+  },
+};
