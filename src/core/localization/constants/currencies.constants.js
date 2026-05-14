30b51692a1233c5e06f9b7560405f402360d2c75
abcp8844-source <abcp8844@gmail.com>
2026-05-14 13:22:41 +0700

    Create currencies.constants.js

---
+++ src/core/localization/constants/currencies.constants.js
@@ -0,0 +1,37 @@
+export const CURRENCIES = {
+  THA: {
+    code: "THB",
+    symbol: "฿",
+    name: "Thai Baht",
+  },
+
+  CHN: {
+    code: "CNY",
+    symbol: "¥",
+    name: "Chinese Yuan",
+  },
+
+  SGP: {
+    code: "SGD",
+    symbol: "S$",
+    name: "Singapore Dollar",
+  },
+
+  TUR: {
+    code: "TRY",
+    symbol: "₺",
+    name: "Turkish Lira",
+  },
+
+  USA: {
+    code: "USD",
+    symbol: "$",
+    name: "US Dollar",
+  },
+
+  GBR: {
+    code: "GBP",
+    symbol: "£",
+    name: "British Pound",
+  },
+};
